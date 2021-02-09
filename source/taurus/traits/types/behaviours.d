69604e19d6bdc355e4a59b60e9d509ab136e3f5b
João Lourenço <jlourenco5691@gmail.com>
2021-02-09 20:04:37 +0000

    traits: behaviours: add isSomeFunction
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

--- source/taurus/traits/types/behaviours.d
+++ source/taurus/traits/types/behaviours.d
@@ -33,3 +33,74 @@ unittest
 
 	static assert(!isIterable!uint);
 }
+
+
+/**
+ * Detect whether symbol or type `T` is a function, a function pointer or a delegate.
+ *
+ * Params: T = type to check.
+ *
+ * Returns: a `bool`.
+ */
+template isSomeFunction(T ...)
+	if (T.length == 1)
+{
+	static if ((is(typeof(& T[0]) U : U*) && is(U == function)) || is(typeof(& T[0]) U == delegate))
+	{
+		// T is a (nested) function symbol.
+		enum bool isSomeFunction = true;
+	}
+	else static if (is(T[0] W) || is(typeof(T[0]) W))
+	{
+		// T is an expression or a type.  Take the type of it and examine.
+		static if (is(W F : F*) && is(F == function))
+			enum bool isSomeFunction = true; // function pointer
+		else
+			enum bool isSomeFunction = is(W == function) || is(W == delegate);
+	}
+	else
+		enum bool isSomeFunction = false;
+}
+
+///
+@safe pure nothrow @nogc
+unittest
+{
+	static real func(ref int) { return 0; }
+	static void prop() @property { }
+	class C
+	{
+		real method(ref int) { return 0; }
+		real prop() @property { return 0; }
+	}
+	scope c = new C;
+	auto fp = &func;
+	auto dg = &c.method;
+	real val;
+
+	static assert( isSomeFunction!func);
+	static assert( isSomeFunction!prop);
+	static assert( isSomeFunction!(C.method));
+	static assert( isSomeFunction!(C.prop));
+	static assert( isSomeFunction!(c.prop));
+	static assert( isSomeFunction!(c.prop));
+	static assert( isSomeFunction!fp);
+	static assert( isSomeFunction!dg);
+
+	static assert(!isSomeFunction!int);
+	static assert(!isSomeFunction!val);
+}
+
+///
+@safe pure nothrow @nogc
+unittest
+{
+	void nestedFunc() { }
+	void nestedProp() @property { }
+	static assert(isSomeFunction!nestedFunc);
+	static assert(isSomeFunction!nestedProp);
+	static assert(isSomeFunction!(real function(ref int)));
+	static assert(isSomeFunction!(real delegate(ref int)));
+	static assert(isSomeFunction!((int a) { return a; }));
+	static assert(!isSomeFunction!isSomeFunction);
+}
